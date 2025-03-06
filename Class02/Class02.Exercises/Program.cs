cf466b9827714cd02b65becb310035b1357f085b
Sandra Simjanoska <sandra.simjanoska@live.com>
2025-03-06 10:47:27 +0100

    Homework

---
+++ Class02/Class02.Exercises/Program.cs
@@ -0,0 +1,69 @@
+﻿#region Exercise1
+//Solve the following problem. On one tree there are 12
+//branches with n apples on them. One basket can hold m
+//apples. If a user enters number of trees find out how
+//many baskets does it take to collect all the apples from
+//the trees?
+//● n = 8
+//● m = 5
+//Result = ?
+
+int n = 8;
+int m = 5;
+int branchesPerTree = 12;
+
+Console.WriteLine("Enter the number of trees: ");
+int numberOfTrees = int.Parse(Console.ReadLine());
+
+int totalApples = numberOfTrees * branchesPerTree * n;
+int basketsNeeded = (totalApples + m - 1) / m;
+
+Console.WriteLine("Total baskets needed: " + basketsNeeded);
+#endregion
+#region Exercise2
+//Create two variables and initialize them
+//● Write code that can test which number is larger
+//● Write code that can test the numbers whether they are
+//even or odd
+//Output:
+//● Write in the console: The larger number from the two is
+//#Number#
+//● After that write: And the number is even/odd
+
+int num1 = 10;
+int num2 = 20;
+
+int largerNumber = (num1 > num2) ? num1 : num2;
+
+string evenOrOdd = (largerNumber % 2 == 0) ? "even" : "odd";
+
+Console.WriteLine("The larger number from the two is " + largerNumber);
+Console.WriteLine("And the number is " + evenOrOdd);
+#endregion
+#region Exercise3
+//Ask the user to enter a number from 1 to 3
+//● If the user enters “1” write “You got a new car!”
+//● If the user enters “2” write “You got a new plane!”
+//● If the user enter “3” write “You got a new bike!”
+//● If the user enters a wrong number or character write an
+//error message in the console
+
+Console.Write("Enter a number from 1 to 3: ");
+string userInput = Console.ReadLine();
+
+switch (userInput)
+{
+    case "1":
+        Console.WriteLine("You got a new car!");
+        break;
+    case "2":
+        Console.WriteLine("You got a new plane!");
+        break;
+    case "3":
+        Console.WriteLine("You got a new bike!");
+        break;
+    default:
+        Console.WriteLine("Error: Invalid input. Please enter a number from 1 to 3.");
+        break;
+}
+#endregion
---
+++ Class02/Class02.Homework/Program.cs
@@ -0,0 +1,111 @@
+﻿#region Task1 ConsoleApp: RealCalculator
+//Create new console application called“RealCalculator” that takes two numbers
+//from the input and asks what operation would the user want to be done ( +, - , * , / ). Then it returns the result.
+
+//Test Data:
+//Enter the First number: 10
+//Enter the Second number: 15
+//Enter the Operation: +
+//Expected Output:
+//The result is: 25
+
+using System.Diagnostics.Metrics;
+using System;
+using static System.Net.Mime.MediaTypeNames;
+using System.Collections.Generic;
+
+Console.Write("Enter the first number: ");
+double num1 = double.Parse(Console.ReadLine());
+
+Console.Write("Enter the second number: ");
+double num2 = double.Parse(Console.ReadLine());
+
+Console.Write("Enter the Operation (+, -, *, /): ");
+char charOperation = Console.ReadLine()[0];
+
+double result = 0;
+bool isValidCharOperation = true;
+
+switch (charOperation)
+{
+    case '+':
+        result = num1 + num2;
+        break;
+    case '-':
+        result = num1 - num2;
+        break;
+    case '*':
+        result = num1 * num2;
+        break;
+    case '/':
+        if (num2 != 0)
+            result = num1 / num2;
+        else
+        {
+            Console.WriteLine("Error: You can not devide by 0.");
+            isValidCharOperation = false;
+        }
+        break;
+    default:
+        Console.WriteLine("Error! Please enter +, -, *, or /.");
+        isValidCharOperation = false;
+        break;
+}
+
+if (isValidCharOperation)
+    Console.WriteLine("The result is: " + result);
+#endregion
+#region Task2 ConsoleApp: AverageNumber
+//Create new console application called “AverageNumber” that takes four numbers as input to calculate and print the average.
+
+//Test Data:
+//Enter the First number: 10
+//Enter the Second number: 15
+//Enter the third number: 20
+//Enter the four number: 30
+//Expected Output:
+//The average of 10, 15, 20 and 30 is: 18
+
+Console.Write("Enter the First number: ");
+double number1 = double.Parse(Console.ReadLine());
+
+Console.Write("Enter the Second number: ");
+double number2 = double.Parse(Console.ReadLine());
+
+Console.Write("Enter the third number: ");
+double number3 = double.Parse(Console.ReadLine());
+
+Console.Write("Enter the fourth number: ");
+double number4 = double.Parse(Console.ReadLine());
+
+double average = (number1 + number2 + number3 + number4) / 4;
+
+Console.WriteLine($"The average of {number1}, {number2}, {number3} and {number4} is: {(int)average}");
+#endregion
+#region Task3 ConsoleApp: SwapNumbers
+//Create new console application called “SwapNumbers”. Input 2 numbers from the console and then
+//swap the values of the variables so that the first variable has the second value and the second variable the first value.
+//Please find example below:
+
+//Test Data:
+//Input the First Number: 5
+//Input the Second Number: 8
+//Expected Output:
+//After Swapping:
+//First Number: 8
+//Second Number: 5
+
+Console.Write("Enter the first number: ");
+int firstNumber = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the second number: ");
+int secondNumber = int.Parse(Console.ReadLine());
+
+int temporaryHolder = firstNumber;
+firstNumber = secondNumber;
+secondNumber = temporaryHolder;
+
+Console.WriteLine("After swapping:");
+Console.WriteLine("First number: " + firstNumber);
+Console.WriteLine("Second number: " + secondNumber);
+#endregion
